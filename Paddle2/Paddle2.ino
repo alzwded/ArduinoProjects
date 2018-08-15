fa1181f4745c46d8107a580e37f214894a73dbae
Vlad <alzwded@gmail.com>
2018-08-15 12:13:08 +0300

    Formatting

--- Paddle2/Paddle2.ino
+++ Paddle2/Paddle2.ino
@@ -1,44 +1,44 @@
 /*
-D2 ------- POT -x
+  D2 ------- POT -x
             |
-A0 -- R1 -- C1
+  A0 -- R1 -- C1
             |
-GND --------+
+  GND --------+
 
-D7 -- X1 -- GND
+  D7 -- X1 -- GND
 
-C1 = C2 = 4.7n
-R1 = R2 = 1k
+  C1 = C2 = 4.7n
+  R1 = R2 = 1k
 
-Paddle controller:
-POT = 1M
-X1 = push button
+  Paddle controller:
+  POT = 1M
+  X1 = push button
 
-when POT is fully turned right, R(D2, C1) = 0Ohm
+  when POT is fully turned right, R(D2, C1) = 0Ohm
 
-Connect DB9:
+  Connect DB9:
 
                      PORT1
                      PORT2
-D7 --             -> PORT3
-D4 --             -> PORT4
-A1 -- R2 -- C2 -- -> PORT5
+  D7 --             -> PORT3
+  D4 --             -> PORT4
+  A1 -- R2 -- C2 -- -> PORT5
                      PORT6
-D2 --             -> PORT7
-GND -             -> PORT8
-A0 -- R1 -- C1 -- -> PORT9
+  D2 --             -> PORT7
+  GND -             -> PORT8
+  A0 -- R1 -- C1 -- -> PORT9
 
-C1 -- GND
-C2 -- GND
+  C1 -- GND
+  C2 -- GND
 
 */
 
 /*
-Right now it only dumps the state of the controls over USB serial.
+  Right now it only dumps the state of the controls over USB serial.
 
-TODO
-* import joystick library
-* improve timing
+  TODO
+  import joystick library
+  improve timing
 */
 
 static const int magic = 6;
@@ -46,7 +46,7 @@ static const int magic = 6;
 int ra, /* read paddle */
     rb, /* read button */
     rc, /* counter of left paddle */
-  rd; /* state (read analogue?) */
+    rd; /* state (read analogue?) */
 int paddles[2], buttons;
 
 void setup()
@@ -66,28 +66,28 @@ void loop()
 {
   rd = (rd + 1) % magic;
   buttons = 0;
-  if(rd == 0 || rd == magic / 2) {
-    paddles[rd / (magic/2)] = 0;
-  	digitalWrite(2, HIGH);
-  	rc = 0;
-  	do {
-      ra = digitalRead((rd / (magic/2)) ? A1 : A0);
-      if(!ra) {
+  if (rd == 0 || rd == magic / 2) {
+    paddles[rd / (magic / 2)] = 0;
+    digitalWrite(2, HIGH);
+    rc = 0;
+    do {
+      ra = digitalRead((rd / (magic / 2)) ? A1 : A0);
+      if (!ra) {
         ++rc;
       }
       else
       {
-        paddles[rd / (magic/2)] = rc;
+        paddles[rd / (magic / 2)] = rc;
         digitalWrite(2, LOW);
         break;
       }
-  	} while(1);
+    } while (1);
   }
   ra = !digitalRead(7);
   buttons |= ra << 1;
   ra = !digitalRead(4);
   buttons |= ra;
-  
+
   Serial.print(paddles[0]);
   Serial.print(' ');
   Serial.print(paddles[1]);
