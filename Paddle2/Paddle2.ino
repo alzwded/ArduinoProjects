e258bf59aba7ab4f1db636d65293e20d6c5b5bc0
Vlad <alzwded@gmail.com>
2018-08-18 18:15:37 +0300

    Add some series resistors; minor code updates
    * force registers where there's no memory involved
    * add ifdefs to toggle debug<->joystick mode
    * get rid of some global variables

--- Paddle2/Paddle2.ino
+++ Paddle2/Paddle2.ino
@@ -37,6 +37,10 @@
 
 */
 
+#define SERIAL_DUMP 1
+#define HID_DEVICE 2
+#define OUTPUT SERIAL_DUMP
+
 // cycles to wait between pot reads
 // it can take a long time to discharge the pot
 static const byte magic = 2;
@@ -47,18 +51,18 @@ static const byte magic2 = magic * 2;
 static const byte paddlePins[2] = { A0, A1 };
 static const byte buttonPins[2] = {  4,  7 };
 
-byte ra, /* accumulator */
-     rb, /* paddle index */
-     rc, /* counter */
-     rd; /* state (read analogue?) */
+byte rd; /* state (read analogue?) */
 byte started; /* started writing to USB */
 byte buttons; /* button state */
 int paddles[2], mins[2], maxs[2]; /* paddle state */
 
 inline void MyPrint(byte rc)
 {
-#if 1
+#if OUTPUT == SERIAL_DUMP
   Serial.print(paddles[rc]); Serial.print('|');
+#elif OUTPUT == HID_DEVICE
+#else
+# error "OUTPUT mode not defined"
 #endif
   // use FPU real quick for better math
   // normalize values from 0-1023
@@ -77,39 +81,47 @@ inline void MyPrint(byte rc)
   // i.e. add a low pass filter on the values.
   // we still have memory and cycles, right?
 
-#if 1
+#if OUTPUT == SERIAL_DUMP
   static char buffer[] = "0000?\t";
 
+  register unsigned char ra asm("r14");
   ra = 4;
   while (ra--) {
-    rb = r16 % 10;
+    register unsigned char rb = r16 % 10;
     buffer[ra] = 48 + rb;
     r16 /= 10;
   }
   buffer[4] = (' ' + ((buttons & (1 << rc)) != 0));
 
   Serial.print(buffer);
-#else
+#elif OUTPUT == HID_DEVICE
   // TODO write as HID joystick
   // Right now it only dumps the state of the controls over USB serial.
+#else
+# error "Output not defined"
 #endif
 }
 
 void setup()
 {
+  // trigger for RC networks
   pinMode(2, OUTPUT);
   digitalWrite(2, LOW);
-  for (rc = 0; rc < 2; ++rc) {
+  // intialize paddle pins
+  for (register unsigned char rc = 0; rc < 2; ++rc) {
     pinMode(paddlePins[rc], INPUT);
     pinMode(buttonPins[rc], INPUT_PULLUP);
   }
 
+  // state
   buttons = 0;
   paddles[0] = paddles[1] = 0;
   mins[0] = mins[1] = 10;
   maxs[0] = maxs[1] = 190;
+  // only start acting as a HID device after pushing a button
   started = 0;
 
+  // setup TCNT1
   TCCR1A = 0; // normal mode; no OC, no PWM
   /* CS12 CS11 CS10
      0    0    0      off
@@ -120,28 +132,32 @@ void setup()
      1    0    1      1/1024
      1    1    0      ext, falling edge
      1    1    1      ext, rising edge
-
   */
+  // 1/64 prescaler gives good values ~0..~1000
   TCCR1B = (1 << CS11) | (1 << CS10);
   TCCR1C = 0; // no force output compare
 
-  TIMSK1 = 0; // no interrupts
+  TIMSK1 = 0; // no timer interrupts
 
-
-#if 1
+#if OUTPUT == SERIAL_DUMP
   Serial.begin(9600);
+#elif OUTPUT == HID_DEVICE
+#else
+# error "Output not defined"
 #endif
 }
 
 void loop()
 {
+  register unsigned char ra asm("r12"); // accumulator
+  
   // increment state
   rd = (rd + 1) % magic2;
 
   // check if it's a cycle where we're reading a pot
   if (rd % magic) {
     // prepare for reading pots
-    rc = 0;
+    register unsigned char rb asm("r13"); // index of joystick we're measuring on this cycle
     rb = rd / magic;
     paddles[rb] = 0;
 
@@ -162,9 +178,13 @@ void loop()
       // read pin state
       ra = digitalRead(paddlePins[rb]);
       // did we hit 67%? (ish; TTL level should be there about)
+      //
       // normally pin held high by schmitt trigger
       // so it gets set low once the cap rises past the
       // schmitt trigger's trigger level
+      //
+      // the data sheet says ~2.4V or so, which puts us well
+      // within the linear ramp-up for the RC network
     } while (ra);
     // quickly pull out the value of TCNT1
     paddles[rb] = TCNT1;
@@ -187,12 +207,12 @@ void loop()
   ra = !digitalRead(buttonPins[1]);
   buttons |= ra;
 
-  if (started = started || buttons) {
+  if (started = (started || buttons)) {
     // output
     MyPrint(0);
     MyPrint(1);
     Serial.println();
   }
 
-  delay(6); // Wait for 6ms; roughly enough to discharge cap (-ish)
+  delay(5); // Wait for 5ms; roughly enough to discharge cap (-ish); discharge enough, that is
 }
